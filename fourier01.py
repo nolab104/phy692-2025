736b372ae0f44f74f5f3075995391b2b7f831a68
Jayasurya <none>
2025-02-09 17:16:40 +0530

    A few annotations

--- fourier01.py
+++ fourier01.py
@@ -2,20 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy.fft import fft, ifft, fftshift
 
+# this is the number of points used to plot the analytically calculated Fourier Transform
 N0 = 2048
 tRange = 8 * np.arange(-N0, N0) * (1 / N0)
 fRange = 0.125 * np.arange(-N0, N0) * (1 / (len(tRange) / N0))
-
 tau = 0.25
 g = np.exp(-(tRange**2) / tau**2)
 G = np.exp(-np.pi**2 * fRange**2 * tau**2) * tau * np.sqrt(np.pi)
 
-plt.figure(figsize=(8, 4.5))
-plt.subplot(2, 1, 1)
-plt.plot(tRange, g, color="midnightblue", label = r"function $g(t)$")
-plt.xlabel("$t$ (s)")
-plt.ylabel("$g(t)$")
-
+# Number of samples 
 N = 24
 tStep = 2 / N
 tSpan = N * tStep
@@ -33,16 +28,24 @@ for i in range(N):
     gReconst = gReconst + GSamplesCalc[i] * np.exp(
         1j * 2 * np.pi * fSamples[i] * tRange
     )
-
 GReconst = np.zeros_like(tRange, dtype=complex)
 for i in range(N):
     GReconst = GReconst + gSamples[i] * np.exp(-1j * 2 * np.pi * tSamples[i] * fRange)*tStep
 
+# Plot 
+plt.figure(figsize=(8, 4.5))
+
+# Time domain 
+plt.subplot(2, 1, 1)
+plt.plot(tRange, g, color="midnightblue", label = r"function $g(t)$")
+plt.xlabel("$t$ (s)")
+plt.ylabel("$g(t)$")
 plt.plot(tSamples, gSamples, "o", color="firebrick", markersize=6, label = "Samples")
 # plt.plot(tRange, np.real((gReconst*fStep)))
 plt.xlim(-2.0, 2.0)
 plt.legend()
 
+# Frequency domain
 plt.subplot(2, 1, 2)
 plt.plot(fRange, np.abs(G), label = "Fourier transform")
 plt.plot(fRange, np.abs(GReconst), label = "DTFT")
